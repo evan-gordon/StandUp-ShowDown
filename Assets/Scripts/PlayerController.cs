a618441569785a335c7edf6fc53a091a259691e3
Alex Estrada <estrada.alex20@gmail.com>
2024-01-27 22:50:02 -0800

    Fixed Camera issue
    Camera didn't like when you looked behind yourself, should be fine now.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -27,13 +28,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(
-            Input.GetAxis("Horizontal") * speed * Time.deltaTime,
-            0,
-            Input.GetAxis("Vertical") * speed * Time.deltaTime
-        );
-
-
+        transform.position = transform.position + GetComponentInChildren<Camera>().transform.right * speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        transform.position = transform.position + GetComponentInChildren<Camera>().transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
     }
 
     // Used by Pickup.cs
