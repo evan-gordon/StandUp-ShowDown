cf60391462e4c9e4ea786ac22083d7963b03b7e0
mcintyrer1 <mcintyrer1@duq.edu>
2024-01-27 20:53:03 -0800

    Added SetRequest to NPC object

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -4,11 +4,25 @@ public class NPC
     public int prefabNum { get; private set; }
     public int[] acceptableEmoji { get; private set; }
 
+    public Request activeRequest { get; private set; }
+
     public NPC(string name, int prefabNum, int[] acceptableEmoji)
     {
         this.name = name;
         this.prefabNum = prefabNum;
         this.acceptableEmoji = acceptableEmoji;
+        this.activeRequest = null;
+    }
+
+    // Returns true if request is successfully set, false otherwise.
+    public bool SetRequest(Request request)
+    {
+        if (activeRequest == null)
+        {
+            activeRequest = request;
+            return true;
+        }
+        return false;
     }
 }
 
