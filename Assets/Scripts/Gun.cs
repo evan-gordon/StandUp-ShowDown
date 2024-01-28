83c927e19427b7276bd108313d96fee9d69c0291
Alex Estrada <estrada.alex20@gmail.com>
2024-01-28 14:17:08 -0800

    add gun charging
    couldn't get a cool way of showing charge so I just made the mic get bigger d:

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,33 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class Gun : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] public float cooldown = 1.0f;
-    private float cooldownTimer;
+    [SerializeField] public float charge = 0.0f;
+    [SerializeField] public float chargeGrowSpeed = 0.3f;
+    public Vector3 OriginalPos;
+    //private float cooldownTimer;
 
     public void Start()
     {
-        cooldownTimer = 0.0f;
+        //cooldownTimer = 0.0f;
     }
     public void Update()
     {
-        cooldownTimer += Time.deltaTime;
-        if (Input.GetMouseButton(0) & cooldownTimer > cooldown)
+        //cooldownTimer += Time.deltaTime;
+        if (Input.GetMouseButton(0) )//& cooldownTimer > cooldown)
         {
-            Shoot();
-            cooldownTimer = 0.0f;
+            charge += Time.deltaTime;
+            //cooldownTimer = 0.0f;
         }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Shoot(charge);
+            charge = 0.0f;
+        }
+        transform.localScale = new Vector3(1 + charge*chargeGrowSpeed, 1+charge * chargeGrowSpeed, 1+charge * chargeGrowSpeed);
     }
-    public void Shoot()
+    public void Shoot(float charge)
     {
         if (PlayerController.instance.canShoot) {
             GameObject b = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
             b.GetComponentInChildren<Bullet>().direction = PlayerController.playerCamera.transform.forward;
             b.GetComponentInChildren<Bullet>().BulletImage = PlayerController.instance.currentBullet;
+            b.GetComponentInChildren<Bullet>().speed *= charge;
             PlayerController.instance.canShoot = false;
         }
     }
